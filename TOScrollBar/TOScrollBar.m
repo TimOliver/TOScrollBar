4b0f7a71ac96f1d9fdabd185c169408a979ed427
Tim Oliver <me@timoliver.com.au>
2016-11-05 20:48:38 +0100

    Added initial graphics and layout

---
+++ TOScrollBar/TOScrollBar.m
@@ -0,0 +1,147 @@
+//
+//  TOScrollBar.m
+//
+//  Copyright 2016 Timothy Oliver. All rights reserved.
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+#import "TOScrollBar.h"
+
+static const CGFloat kTOScrollBarTrackWidth = 2.0f;
+static const CGFloat kTOScrollBarHandleWidth = 4.0f;
+static const CGFloat kTOScrollBarHandleMinHeight = 70.0f;
+static const CGFloat kTOScrollBarWidth = 44.0f;
+static const CGFloat kTOScrollBarMaxHeight = 450.0f;
+static const CGFloat kTOScrollBarVerticalPadding = 20.0f;
+
+@interface TOScrollBar ()
+
+@property (nonatomic, weak) UIScrollView *scrollView;
+
+@property (nonatomic, strong) UIImageView *trackView; // The track indicating the scrollable distance
+@property (nonatomic, strong) UIImageView *handleView; // The handle that may be dragged in the scroll bar
+
+- (void)setUp;
+- (void)layoutInScrollView;
+
++ (UIImage *)verticalCapsuleImageWithWidth:(CGFloat)width;
+
+@end
+
+@implementation TOScrollBar
+
+- (void)setUp
+{
+    if (!self.trackView) {
+        self.trackView = [[UIImageView alloc] initWithImage:[TOScrollBar verticalCapsuleImageWithWidth:kTOScrollBarTrackWidth]];
+        self.trackView.tintColor = [UIColor colorWithWhite:0.0f alpha:0.1f];
+        [self addSubview:self.trackView];
+    }
+    
+    if (!self.handleView) {
+        self.handleView = [[UIImageView alloc] initWithImage:[TOScrollBar verticalCapsuleImageWithWidth:kTOScrollBarHandleWidth]];
+        [self addSubview:self.handleView];
+    }
+}
+
+- (void)layoutSubviews
+{
+    CGRect frame = CGRectZero;
+    frame.size.width = kTOScrollBarTrackWidth;
+    frame.size.height = self.frame.size.height;
+    frame.origin.x = (self.frame.size.width - kTOScrollBarTrackWidth) * 0.5f;
+    self.trackView.frame = CGRectIntegral(frame);
+    
+    frame = CGRectZero;
+    frame.size.width = kTOScrollBarHandleWidth;
+    frame.size.height = 50.0f;
+    frame.origin.x = (self.frame.size.width - kTOScrollBarHandleWidth) * 0.5f;
+    self.handleView.frame = CGRectIntegral(frame);
+}
+
+- (void)layoutInScrollView
+{
+    CGRect scrollViewFrame = self.scrollView.frame;
+    UIEdgeInsets insets = self.scrollView.contentInset;
+    CGPoint contentOffset = self.scrollView.contentOffset;
+    
+    scrollViewFrame.size.height -= (insets.top + insets.bottom);
+    CGFloat height = scrollViewFrame.size.height - (kTOScrollBarVerticalPadding * 2);
+    
+    CGRect frame = CGRectZero;
+    frame.size.width = kTOScrollBarWidth;
+    frame.size.height = MIN(height, kTOScrollBarMaxHeight);
+    frame.origin.x = scrollViewFrame.size.width - kTOScrollBarWidth;
+    
+    frame.origin.y = (scrollViewFrame.size.height - frame.size.height) * 0.5f;
+    frame.origin.y += contentOffset.y;
+    frame.origin.y += self.scrollView.contentInset.top;
+    
+    self.frame = frame;
+}
+
+- (void)willMoveToSuperview:(UIView *)newSuperview
+{
+    [super willMoveToSuperview:newSuperview];
+    [self setUp];
+}
+
+- (void)observeValueForKeyPath:(NSString *)keyPath ofObject:(id)object
+                        change:(NSDictionary<NSKeyValueChangeKey,id> *)change context:(void *)context
+{
+    [self layoutInScrollView];
+}
+
+- (void)addToScrollView:(UIScrollView *)scrollView
+{
+    if (scrollView == self.scrollView) {
+        return;
+    }
+    
+    [self.scrollView removeObserver:self forKeyPath:@"contentOffset"];
+    [self.scrollView removeObserver:self forKeyPath:@"frame"];
+    self.scrollView = scrollView;
+    [self.scrollView addObserver:self forKeyPath:@"contentOffset" options:NSKeyValueObservingOptionNew context:nil];
+    [self.scrollView addObserver:self forKeyPath:@"frame" options:NSKeyValueObservingOptionNew context:nil];
+    self.scrollView.showsVerticalScrollIndicator = NO;
+    
+    [self.scrollView addSubview:self];
+    
+    [self.scrollView.panGestureRecognizer requireGestureRecognizerToFail:self.panGestureRecognizer];
+    [self layoutInScrollView];
+}
+
+#pragma mark - Image Generation -
++ (UIImage *)verticalCapsuleImageWithWidth:(CGFloat)width
+{
+    UIImage *image = nil;
+    CGFloat radius = floor(width * 0.5f);
+    CGRect frame = (CGRect){0, 0, width, width+1};
+    
+    UIGraphicsBeginImageContextWithOptions(frame.size, NO, 0.0f);
+    [[UIBezierPath bezierPathWithRoundedRect:frame cornerRadius:radius] fill];
+    image = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    
+    image = [image resizableImageWithCapInsets:UIEdgeInsetsMake(radius, 0, radius, 0)];
+    image = [image imageWithRenderingMode:UIImageRenderingModeAlwaysTemplate];
+    
+    return image;
+}
+
+@end
--- TOScrollBarExample/ViewController.m
+++ TOScrollBarExample/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import "TOScrollBar.h"
 
 @interface ViewController ()
 
@@ -16,13 +17,9 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
-}
-
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    
+    TOScrollBar *scrollBar = [[TOScrollBar alloc] init];
+    [scrollBar addToScrollView:self.tableView];
 }
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
