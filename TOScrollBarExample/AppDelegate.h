4b23aca11a42f81d92f4cd00f1cdb815dea87d60
Tim Oliver <me@timoliver.com.au>
2016-11-05 17:52:47 +0100

    Initial project creation

---
+++ TOScrollBarExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TOScrollBarExample
+//
+//  Created by Tim Oliver on 5/11/16.
+//  Copyright © 2016 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TOScrollBarExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TOScrollBarExample
+//
+//  Created by Tim Oliver on 5/11/16.
+//  Copyright © 2016 Tim Oliver. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
