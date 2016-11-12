8b6185cb5ae2b6cc70aa8812be26d239e3d75368
Tim Oliver <me@timoliver.com.au>
2016-11-12 14:47:16 -0800

    Reduced number of demo cells to semi-realistic number

--- TOScrollBarExample/ViewController.m
+++ TOScrollBarExample/ViewController.m
@@ -25,7 +25,7 @@
 
 - (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section
 {
-    return 10000;
+    return 1000;
 }
 
 - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath
