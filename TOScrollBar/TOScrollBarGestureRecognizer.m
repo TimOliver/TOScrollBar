5dc1aef3336d099d07c62e928c685c1263211fa3
Tim Oliver <me@timoliver.com.au>
2017-12-11 18:11:41 +0800

    Updated license headers

--- TOScrollBar/TOScrollBarGestureRecognizer.m
+++ TOScrollBar/TOScrollBarGestureRecognizer.m
@@ -1,14 +1,27 @@
 //
-//  TOScrollBarGestureRecognizer.m
-//  TOScrollBarExample
+//  TOScrollBarGestureRecognizer.h
 //
-//  Created by Tim Oliver on 12/9/17.
-//  Copyright © 2017 Tim Oliver. All rights reserved.
+//  Copyright 2017 Timothy Oliver. All rights reserved.
 //
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to
+//  deal in the Software without restriction, including without limitation the
+//  rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+//  sell copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in
+//  all copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+//  OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+//  WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR
+//  IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
 #import "TOScrollBarGestureRecognizer.h"
 #import <UIKit/UIGestureRecognizerSubclass.h>
-
 #import "TOScrollBar.h"
 
 @interface TOScrollBarGestureRecognizer ()
@@ -22,6 +35,7 @@
 #pragma mark - Gesture Recognizer Filtering -
 - (BOOL)canPreventGestureRecognizer:(UIGestureRecognizer *)preventedGestureRecognizer
 {
+    // Ensure that the pan gesture recognizer from the scroll view doesn't override the scroll bar
     UIView *view = preventedGestureRecognizer.view;
     if ([view isEqual:self.scrollBar.scrollView]) {
         return YES;
@@ -54,10 +68,7 @@
 #pragma mark - Accessors -
 - (TOScrollBar *)scrollBar
 {
-    if ([self.view isKindOfClass:[TOScrollBar class]] == NO) {
-        return nil;
-    }
-
+    if ([self.view isKindOfClass:[TOScrollBar class]] == NO) { return nil; }
     return (TOScrollBar *)self.view;
 }
 
